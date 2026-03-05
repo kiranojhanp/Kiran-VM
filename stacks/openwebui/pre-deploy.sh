6e15f114d70c0b28fd52a27fc951ffa46d250ed6
Kiran Ojha <kiranojha1226@gmail.com>
2026-03-05 18:09:25 +0800

    chore: remove pre-deploy.sh

--- stacks/openwebui/pre-deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Komodo Pre Deploy script for openwebui stack.
-# Copies the stable .env (written by Ansible, survives git pull) into the
-# compose working directory before docker compose up runs.
-set -e
-cp /opt/openwebui/.env /opt/komodo/periphery/repos/kiran-vm/stacks/openwebui/.env
